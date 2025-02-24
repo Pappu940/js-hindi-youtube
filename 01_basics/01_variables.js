3099abfc4eab43345e0bebfad27fe1573c125ce6
Pappu940 <pappusingh232004@gmail.com>
2025-02-24 03:30:51 +0000

    change config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 123
+let accountEmail = "pappu307@gmail;.com"
+accountPassword = "1234"
+accountCity = "Bhopal"
+/*
+prefer not to use var
+Because of issue in block and functional scope
+*/
+
+console.table([accountEmail, accountPassword, accountCity])
